eb1f2af05aac89991fc820b4f2df557e11a5700e
Christoph Lemannczick <info@lem-solutions.de>
2026-03-25 20:32:48 +0100

    Bloomfilter im Scanner

---
+++ src/metadaten.rs
@@ -0,0 +1,16 @@
+
+
+
+async fn info_hash_mit_knoten_verarbeiten(info: InfoHashMitKnoten) {
+	
+}
+
+async fn info_hashes_mit_knoten_verarbeiten(info: InfoHashesMitKnoten) {
+	
+}
+
+async fn info_hash_mit_peer_verarbeiten(info: (U160, SocketAddr)) {
+	
+}
+
+// TODO
--- src/scanner.rs
+++ src/scanner.rs
@@ -6,6 +6,7 @@ use std::time::{Duration, Instant, SystemTime};
 use metrics::gauge;
 use smol::net::{SocketAddr, SocketAddrV4, SocketAddrV6};
 use smol::{Timer, channel::*, stream::Stream};
+use bloomfilter::Bloom;
 
 use crate::addr_generisch::Addr;
 use crate::dht_knoten::Anfrageergebnis;
@@ -17,6 +18,9 @@ use crate::{
 	dht_knoten::{DhtKnoten, InfoHashMitKnoten},
 	krpc::{KrpcAnfrage, KrpcAntwort, KrpcFehler, KrpcFehlercode},
 };
+
+const BLOOM_FALSCH_POSITIV_RATE : f64 = 0.05;
+const BLOOM_SCHÄTZUNG_EINTRÄGE_PRO_ZEITGRENZE : usize = 15_000_000;
 
 /// Minimale Wartezeit bevor ein Knoten neu gescannt werden darf. Muss
 /// mindestens 6 Stunden sein.
@@ -30,35 +34,54 @@ const KNOTENPUFFERGRÖßE: usize = 2048;
 const KNOTEN_NACHSCAN_SCHWELLWERT: usize = 1536;
 
 struct KnotenSperrliste {
-	erledigt_a: HashSet<U160>,
-	erledigt_b: HashSet<U160>,
+	erledigt_a: Bloom<U160>,
+	erledigt_b: Option<Bloom<U160>>,
+	anz_erledigt_a: usize,
 	zeitstempel: Instant,
 }
 impl KnotenSperrliste {
 	fn neu() -> Self {
 		KnotenSperrliste {
-			erledigt_a: HashSet::new(),
-			erledigt_b: HashSet::new(),
+			erledigt_a: Bloom::new_for_fp_rate(
+				BLOOM_SCHÄTZUNG_EINTRÄGE_PRO_ZEITGRENZE,
+				BLOOM_FALSCH_POSITIV_RATE).unwrap(),
+			erledigt_b: None,
 			zeitstempel: Instant::now(),
+			anz_erledigt_a: 0,
 		}
 	}
 
 	/// Fügt die gegebene Knoten-Id ein und gibt zurück ob diese bereits vorhanden
 	/// war.
 	fn gesehen(&mut self, id: U160) -> bool {
+		// TODO Diese Schätzung ist nicht ganz genau, da faktisch mehr
+		//      verschiedene ID gesehen wurden als gezählt(wegen der
+		//      Falsch-Positiv Rate). Man könnte ausrechnen wie hoch diese
+		//      statistisch sein müsste.
 		if self.zeitstempel.elapsed() > ZEITGRENZE {
-			std::mem::swap(&mut self.erledigt_a, &mut self.erledigt_b);
-			self.erledigt_a.clear();
+			self.erledigt_b = Some(Bloom::new_for_fp_rate(
+				self.anz_erledigt_a,
+				BLOOM_FALSCH_POSITIV_RATE,
+			).unwrap());
+			std::mem::swap(&mut self.erledigt_a, self.erledigt_b.as_mut().unwrap());
+			
+			self.anz_erledigt_a = 0;
 			self.zeitstempel = Instant::now();
 		}
-		let vorhanden =
-			self.erledigt_a.contains(&id) || self.erledigt_b.contains(&id);
-
-		if !vorhanden {
-			self.erledigt_a.insert(id);
+		
+		// b zuerst, da wie die ID nicht in a einfügen wollen, falls sie schon in b
+		// ist.
+		if let Some(ref b) = self.erledigt_b {
+			return b.check(&id)
+		}
+		
+		if self.erledigt_a.check_and_set(&id) {
+			return true;
+		} else {
+			self.anz_erledigt_a += 1;
 		}
 
-		vorhanden
+		false
 	}
 }
 
